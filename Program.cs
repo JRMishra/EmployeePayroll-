e4ba01364805efa68f8c946961fa4406eecf3fdd
JR Mishra <jrmnitdgp@gmail.com>
2020-11-05 21:37:08 +0530

    [JR Mishra]Add. Project Initialized

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service Program");
+            Console.WriteLine("===========================================");
+        }
+    }
+}
